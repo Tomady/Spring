0e0222229d7448bd2f97415323f41bd8fe7e5d20
Tomady <87797173+Tomady@users.noreply.github.com>
2022-07-23 19:53:38 +0900

    feat(Chapter1): 스프링 개발 환경 구축
    개발을 위한 준비

---
+++ ex00/src/test/java/org/zerock/sample/SampleTests.java
@@ -0,0 +1,11 @@
+package org.zerock.sample;
+
+import lombok.Setter;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleTests {
+
+    @Setter
+    private Restaurant restaurant;
+}
