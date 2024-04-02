b302650144c7eb67f1d612ff0f092d2d65010d78
Tomady <scanliberte@gmail.com>
2024-04-03 02:35:21 +0900

    feat: add @InitBinder DateFormat yyyy-MM-dd, yyyy/MM/dd

---
+++ ex00/src/main/java/org/zerock/domain/TodoDTO.java
@@ -0,0 +1,12 @@
+package org.zerock.domain;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Data
+public class TodoDTO {
+    private String title;
+
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private Data dueDate;
+}
