d08673783529dc36e3b4ca9deb1f68bbb177b40b
Tomady <scanliberte@gmail.com>
2024-06-07 11:07:07 +0900

    update: aop add LogAdvice Around&ProceedingJoinPoint

--- ex04/src/main/java/org/zerock/aop/LogAdvice.java
+++ ex04/src/main/java/org/zerock/aop/LogAdvice.java
@@ -1,10 +1,14 @@
 package org.zerock.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Slf4j
@@ -26,5 +30,28 @@ public class LogAdvice {
     public void logException(Exception exception) {
         log.info("Exception....!!!!");
         log.info("exception: " + exception);
+    }
+
+    @Around("execution(* org.zerock.service.SampleService*.*(..))")
+    public Object logTime(ProceedingJoinPoint pjp) {
+        long start = System.currentTimeMillis();
+
+        log.info("Target: " + pjp.getTarget());
+        log.info("Param: " + Arrays.toString(pjp.getArgs()));
+
+        // invoke method
+        Object result = null;
+
+        try {
+            result = pjp.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        long end = System.currentTimeMillis();
+
+        log.info("TIME: " + (end - start));
+
+        return result;
     }
 }
