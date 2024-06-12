3fa5008c4f9f79e82004e5abd9c1546283c7b588
Tomady <scanliberte@gmail.com>
2024-06-12 09:19:09 +0900

    feat: add reply transactional

---
+++ ex04/src/main/java/org/zerock/domain/BoardVO.java
@@ -0,0 +1,21 @@
+package org.zerock.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@ToString
+public class BoardVO {
+    private int bno;
+    private String title;
+    private String content;
+    private String writer;
+    private Date regdate;
+    private Date updatedate;
+
+    private int replyCnt;
+}
