cd28372d1e9770862aed0e5829b6af9b1a579aec
Tomady <scanliberte@gmail.com>
2024-06-12 15:13:04 +0900

    feat: fileUpload add uuid

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 @Controller
 @Slf4j
@@ -70,6 +71,9 @@ public class UploadController {
             uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 
             log.info("only file name: " + uploadFileName);
+
+            UUID uuid = UUID.randomUUID();
+            uploadFileName = uuid.toString() + "_" + uploadFileName;
 
 //            File saveFile = new File(uploadFolder, uploadFileName);
             File saveFile = new File(uploadPath, uploadFileName);
